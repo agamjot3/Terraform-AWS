ddfe67f98058ab81149e3fc3eeb78d276fb3cb8c
agamjot-nexg <agamjot@nexgsolution.com>
2021-06-19 05:49:26 +0530

    Added File

---
+++ delete-ec2-instance.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region     = "region"
+  access_key = "********"
+  secret_key = "***********"
+}
+
+# resource "aws_instance" "web" {
+#   ami           = "ami-0c1a7f89451184c8b"
+#   instance_type = "t3.xlarge"
+
+#   tags = {
+#     Name = "Big Server"
+#   }
+# }
